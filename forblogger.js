3808ba8e15d3a0d7f58b6446c48504d56910d982
vellaparass <74484561+vellaparass@users.noreply.github.com>
2021-06-08 07:53:06 +0600

    Update forblogger.js

--- forblogger.js
+++ forblogger.js
@@ -1,6 +1,6 @@
 
 $(".postfooter").append("<div class='postscan ripple' title='QR Scane Code'><h2>Read Post by Scan</h2><p>Post Title</p><canvas id='post-qr-code'/><a class='qrcreditmr' href='https://www.mrlaboratory.com'>Provided by MR Laboratory</a></div><div class='linkboxbtn'> <input value='Post Link... '/><button> Copy Link <i aria-hidden='true' class='fa fa-clipboard'/>      </button></div>");
-
+$('head').append('<link rel="stylesheet" type="text/css" href="style1.css">');
 var postlinkv = window.location.href;
 var postTitlev = document.querySelectorAll(".post-title")[0].textContent;
 $(".postscan p").text(postTitlev);
